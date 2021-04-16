652be9a8b8d44d6a419991daf51d9cab0e896484
danielkiko <danielkiko@yandex.ru>
2021-04-16 16:07:50 +0300

    final ver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Note } from './shared/interfaces/note.interface';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +9,25 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'project11';
-  notetitle ='';
-  maintext ='';
-  date = new Date;
+  notetitle = '';
+  maintext = '';
+  notes: Note[]=[];
+  idnumber = 1;
 
-  submit(){
-    console.log(this.notetitle);
-    console.log(this.maintext);
-    this.notetitle ='';
-    this.maintext ='';
-    console.log(this.date.getMonth()+1);
-    console.log(this.date.getDate());
-    console.log(this.date.getHours());
-    console.log(this.date.getMinutes());
-}
+  submit() {
+    let note: Note = { id: this.idnumber++, name: this.notetitle, maintext: this.maintext, date: new Date() }
+    this.notes.push(note);
+    console.log(this.notes);
+    // console.log(this.notetitle);
+    // console.log(this.maintext);
+    // this.notetitle ='';
+    // this.maintext ='';
+    // console.log(this.date.getMonth()+1);
+    // console.log(this.date.getDate());
+    // console.log(this.date.getHours());
+    // console.log(this.date.getMinutes());
+  }
+  delete(index:number) {
+    this.notes.splice(index,1);
+  }
 }
