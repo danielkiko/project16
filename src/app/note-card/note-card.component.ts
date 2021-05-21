97d4d20d499c4d3ec794522db20c51ee65b46107
danielkiko <danielkiko@yandex.ru>
2021-05-21 11:01:32 +0300

    ver1

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Note } from 'src/app/shared/interfaces/note.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Note } from 'app/shared/interfaces/note.interface';
+
+
 
 @Component({
   selector: 'app-card-form',
@@ -8,22 +11,28 @@ import { Note } from 'src/app/shared/interfaces/note.interface';
 })
 export class NoteCardComponent implements OnInit {
     @Input() inputNote: Note;
-  @Output() noteDelete = new EventEmitter<null>();
+  @Output() noteDelete = new EventEmitter<number>();
   @Output() noteEdit = new EventEmitter<Note>();
   status = false;
-
-  constructor() { }
+  NoteForm!: FormGroup
+  constructor(private fb: FormBuilder) { }
   
   ngOnInit(): void {
+    const controls = {
+      name: [null, [Validators.required, Validators.maxLength(100)]],
+      maintext: [null, [Validators.required, Validators.maxLength(100)]],
+      date: Date
+    }
+    this.NoteForm = this.fb.group(controls);
   }
   onDeleteNote() {
-      this.noteDelete.emit();
+      this.noteDelete.emit(this.inputNote.id);
   }
   onEditNote() {
-    this.inputNote.date= new Date;
+    const note = this.NoteForm.value;
       this.status = !this.status;
       if (this.status == false)
-      this.noteEdit.emit();
+      this.noteEdit.emit(note);
   }
 
 }
